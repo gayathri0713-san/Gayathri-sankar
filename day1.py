883ca7cdcc0e1ddbdb7ae45c9b3642486189edb1
gayathri0713-san <gayukav56@gmail.com>
2025-01-30 11:03:13 +0530

    gayathri

---
+++ day1.py
@@ -0,0 +1,7 @@
+lunch=input("how many chapathi are you ate:")
+if 1<5:
+    print("hungry")
+elif 5<10:
+    print("sleep")
+else:
+    print("")
---
+++ list.py
@@ -0,0 +1,11 @@
+"""list=[1,6,3,8,9]
+list.sort()
+print(list)
+
+list=[4,1,8,3,6]
+x=list.count(2)
+print(x)"""
+
+list=["A",-20,"a",94,66]
+x=sorted(list,key=str)
+print(x)
